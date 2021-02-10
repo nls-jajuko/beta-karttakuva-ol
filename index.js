c2b74b124df1c647556da89b3a0bf4b1140d0f41
nls-jajuko <nls-jajuko@users.noreply.github.com>
2021-02-10 07:29:24 +0200

    Update index.js
    remove proj as it's not needed in this quick start

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-
 import 'ol/ol.css';
 import FullScreen from 'ol/control/FullScreen';
 import apply from 'ol-mapbox-style';
-import Hash from './hash';
-import proj4 from 'proj4';
 import Map from 'ol/Map';
-import { register } from 'ol/proj/proj4';
+import Hash from './hash';
 
-
-proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
-proj4.defs("EPSG:3857", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs");
-//proj4.defs("EPSG:3067", "+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
-register(proj4);
-
+/* map style and version selections */
+/* v20 is current published in 2020 */
+/* v21 available in 2021 */
 const tileVer = 'v20', styleVer = 'v20',
+  styleName = 'hobby',
   tileMatrixSet = 'WGS84_Pseudo-Mercator',
+  /* get your own api key at maanmittauslaitos.fi */
   apiKey = '7cd2ddae-9f2e-481c-99d0-404e7bc7a0b2',
-  styleUrl = `https://avoin-karttakuva.maanmittauslaitos.fi/vectortiles/stylejson/${styleVer}/taustakartta.json?TileMatrixSet=${tileMatrixSet}&api-key=${apiKey}`;
+  styleUrl = `https://avoin-karttakuva.maanmittauslaitos.fi/vectortiles/stylejson/${styleVer}/${styleName}.json?TileMatrixSet=${tileMatrixSet}&api-key=${apiKey}`;
 
+/* maplibre compatible location url hash */
 const hash = new Hash(),
   map = new Map({ target: 'map' });
 hash.addTo(map);
 
+/* fetch style and apply to map */
 fetch(styleUrl).then(r => r.json()).then(styleJson => {
   apply(
     map, styleJson).then(function (map) {
